8aa75fab8eec8462a2fad7300944fa9d66733191
Sebastian-Nyberget <nybesebe@gmail.com>
2024-12-02 09:48:05 +0100

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "./components/Hero";
+
 export default function Home() {
   return (
-    <div>
-      test
+    <div className="max-w-7xl w-full px-4 md:px-8 mx-auto">
+      <Hero />
     </div>
   );
 }
